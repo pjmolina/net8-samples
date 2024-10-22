eaca28bffee40edfd17c308eb047f3465f04551d
Pedro J. Molina <pjmolina@metadev.pro>
2024-10-22 14:01:10 +0200

    day 2 samples - group 1

---
+++ net8-group1/HelloWorld/Color.cs
@@ -0,0 +1,29 @@
+﻿namespace HelloWorld
+{
+    public enum Color
+    { 
+        Undefined = 0,
+        Orange = 1,
+        Red = 2,
+        Green = 3,
+        Blue = 4,
+        Yellow = 5,
+        Purple = 123
+    }
+
+    public enum CivilStatus
+    {
+        Single = 'S',
+        Married = 'M',
+    }
+
+
+    public class Car
+    {
+        public string Brand;
+        public Color CarColor = Color.Yellow;
+       
+    }
+
+
+}
---
+++ net8-group1/HelloWorld/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Them main entry point
+    /// </summary>
+    public class Program
+    {
+
+        /* multiline comment
+         */
+
+        /// <summary>
+        /// Add two numbers***
+        /// </summary>
+        /// <param name="a">the first number</param>
+        /// <param name="b">second</param>
+        /// <returns>them sum of the numbers</returns>
+        static int Add(int a, int b)
+        {
+            return a + b;  //single line 
+        }
+
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            car1.Brand = "Honda";
+            car1.CarColor = Color.Blue;
+
+            var status = CivilStatus.Married;
+
+            switch(status) 
+            {
+                case CivilStatus.Single:
+                    break;
+                case CivilStatus.Married:
+                    break;
+            }
+
+
+            char[] letters = [ 'A', 'B', 'C'];
+            int[] numbers = [ 1, 2, 3, 4];
+            int[] nums = { 1, 2, 3, 5, 8 };
+
+            var result2 = nums.Skip(1).Take(3);  //   { 2, 3, 5 }
+
+            double z1 = 9.87;
+            int z2 = (int) z1;  // z2 = 9
+
+            int z3 = (int) Math.Round(z1); // z3 = 10
+
+            // int to string
+            string s1 = z2.ToString();
+
+            // string to int
+            try
+            {
+                int a = int.Parse(s1);
+                // statement 2
+            }
+            catch (FormatException ex)
+            {
+                 // 1
+            }
+            catch (ArgumentNullException ex)
+            {
+                return; // 2
+            }
+            catch
+            {
+                return; // 2
+            }
+            finally
+            { 
+                // END
+            }
+
+            letters.Count(); // 3
+
+            // && AND short-circuit C, C++, Java, C#
+            // || OR  short-circuit
+            //
+            //  AND
+            //  1 AND 1 -> 1
+            //  1 AND 0 -> 0
+            //  0 AND x1 -> 0
+            //  0 AND x0 ->
+            //  
+            //  OR
+            //  1 OR x1 -> 1
+            //  1 OR x0 -> 1
+            //  0 OR 1 -> 1
+            //  0 OR 0 -> 0
+
+            byte b0 = 0x00;
+            byte mask = 0x0F;
+
+            
+
+
+
+
+            var a1 = letters != null;
+            var a2 = letters.Count();
+
+            if (a1 && a2) 
+            {
+                // yes
+            } 
+            else 
+            {
+                // no
+            }
+
+            var currency = "YEN";
+
+            switch (currency.ToLower()) 
+            {
+                case "yen":
+                case "eur":
+                    var result = "10 EUR";
+                    break;
+                case "usd":
+                    var result = "10 USD";
+                    break;
+                default:
+                    var result = "10 " + currency;
+                    break;
+            }
+            // continue
+
+            // C
+            for (var i=0; i < letters.Length; i++) 
+            {
+                var letter = letters[i];
+            }
+            
+            int i = 0;
+            while (i < letters.Length) 
+            {
+                // statements
+                //...
+                i++;
+            }
+            // 0 or more times
+
+            do
+            {
+                // statements
+                //...
+                i++;
+            }
+            while (i < letters.Length);
+            // 1 or more times
+
+            // LINQ
+            // Lambda
+            //foreach (var ch in letters.Where(c => c.Country=="ES") ) 
+            //{
+                
+            //}
+
+
+
+
+
+            char letter = 'A';
+            string someText = "some text";
+            int age = 20;
+            float n1 = 12.34F;  // binary 0.3 -> binar  0.299999 0.30000001
+            double d1 = 12343242.84982394829849D;
+            decimal d2 = 1232344235.234M;  // Binary Decimal Coding
+            bool result = false;
+
+            age = Add(age, 5);
+
+            Console.WriteLine("Hello, World, NET8!");
+            Console.WriteLine("Bye: "  + age);
+        }
+
+
+    }
+}
+
+
+//  HelloWorld          PascalCase
+//  helloWorld          camelCase
+//  hello_world         snake_Case
+//  hello-world         kebabCase
